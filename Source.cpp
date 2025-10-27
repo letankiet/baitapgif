318743a4e2faf6346d069ec546b212479a20ec52
t <letankiet37@gmail.com>
2025-10-27 11:28:34 +0700

    Add Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -8,7 +8,10 @@ float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int a = 5, b = 3;
+    	cout << "Tong: " << tong(a, b) << endl;
+    	cout << "Hieu: " << hieu(a, b) << endl;
+    	cout << "Tich: " << tich(a, b) << endl;
 	system("pause");
 	return 0;
 }
